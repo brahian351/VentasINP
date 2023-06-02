066309fa89af9302712a7bf866cad210d5927575
jhonned01 <jhonned01@gmail.com>
2023-06-02 10:57:52 -0500

    listo el login

--- app/api/Login/route.js
+++ app/api/Login/route.js
@@ -1,11 +1,32 @@
 import connectionPool from "@/config/db";
+import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  const { searchParams } = new URL(req.url);
-  const Usuario = searchParams?.get("Usuario");
-  const Pass = searchParams?.get("Pass");
+  try {
+    const { searchParams } = new URL(req.url);
+    const Usuario = searchParams?.get("Usuario");
+    const Pass = searchParams?.get("Pass");
 
-  const [user] = await connectionPool.query("SELECT * FROM usuarios");
+    if (!Usuario?.length || !Pass?.length) {
+      console.log("entro");
+      return NextResponse.json(
+        { body: "Usiario o Contraseña vacios" },
+        { status: 401 }
+      );
+    }
 
-  console.log("user", user);
+    const [user] = await connectionPool.query(
+      `SELECT * FROM usuarios WHERE nickname = '${Usuario}' AND password = '${Pass}'`
+    );
+
+    if (user.length > 0) {
+      return NextResponse.json({ body: user[0] || {} }, { status: 200 });
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { body: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
